7ac46f8987923bdc8ca445a2e536d32574f407f6
Rume7 <electengrhume@gmail.com>
2023-07-31 21:03:20 +0100

    Cleaned up build dependency

--- src/main/java/com/redislabs/edu/redisearch/demo/RediSearchApplication.java
+++ src/main/java/com/redislabs/edu/redisearch/demo/RediSearchApplication.java
@@ -1,87 +1,27 @@
 package com.redislabs.edu.redisearch.demo;
 
 import com.github.javafaker.Faker;
-import io.redisearch.Query;
-import io.redisearch.SearchResult;
-import io.redisearch.Suggestion;
+import com.redislabs.edu.redisearch.demo.model.Article;
+import com.redislabs.edu.redisearch.demo.repository.ArticleRepository;
+import com.redislabs.edu.redisearch.demo.repository.AuthorRepository;
 import io.redisearch.client.Client;
 import io.redisearch.Schema;
 import io.redisearch.client.IndexDefinition;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Reference;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import redis.clients.jedis.exceptions.JedisDataException;
 
 import java.util.*;
 import java.util.stream.IntStream;
 
+import static com.redislabs.edu.redisearch.demo.config.JedisConfiguration.getFakeArticleTitle;
+
 @SpringBootApplication
 public class RediSearchApplication {
-
-    @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
-
-    @Bean
-    RedisTemplate<String, String> redisTemplate() {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-
-        return template;
-    }
-
-    static Map<ArticleType, String> titles = Map.of(
-        ArticleType.INFLUENCE, "The influence of %s on %s", //
-            ArticleType.LEARN, "What I learn %s from %s", //
-            ArticleType.COOK, "How to cook %s with %s", //
-            ArticleType.TEACH, "Teach your %s how to pilot a %s " //
-    );
-
-    private static String getFakeArticleTitle(Faker faker) {
-        var keys = titles.keySet();
-        var rando = (long) (keys.size() * Math.random());
-        var key = keys.stream().skip(rando).findAny().get();
-        var param1 = "";
-        var param2 = "";
-        switch (key) {
-            case INFLUENCE:
-                param1 = faker.ancient().god();
-                param2 = faker.food().dish();
-                break;
-            case LEARN:
-                param1 = faker.artist().name();
-                param2 = faker.backToTheFuture().character();
-                break;
-            case COOK:
-                param1 = faker.food().dish();
-                param2 = StringUtils.capitalize(faker.animal().name());
-                break;
-            case TEACH:
-                param1 = faker.animal().name();
-                param2 = faker.aviation().aircraft();
-                break;
-        }
-        return String.format(titles.get(key), param1, param2);
-    }
 
     @Bean
     CommandLineRunner makeMeSomeData(ArticleRepository articleRepo, //
@@ -94,7 +34,7 @@ public class RediSearchApplication {
                 var faker = new Faker();
 
                 IntStream.range(0, 250).forEach(n -> {
-                    var author = new Author(null, faker.name().fullName());
+                    var author = new com.redislabs.edu.redisearch.demo.model.Author(null, faker.name().fullName());
                     authorRepo.save(author);
                 });
 
@@ -109,8 +49,8 @@ public class RediSearchApplication {
 
                     var article = new Article(null, articleTitle, Double.parseDouble(faker.commerce().price()), new HashSet<>());
                     IntStream.range(0, random.nextInt(2) + 1).forEach(j -> {
-                        var authorId = redisTemplate.opsForSet().randomMember(Author.class.getName());
-                        article.addAuthor(new Author(authorId, null));
+                        var authorId = redisTemplate.opsForSet().randomMember(com.redislabs.edu.redisearch.demo.model.Author.class.getName());
+                        article.addAuthor(new com.redislabs.edu.redisearch.demo.model.Author(authorId, null));
                     });
                     articleRepo.save(article);
                 });
@@ -139,69 +79,4 @@ public class RediSearchApplication {
     public static void main(String[] args) {
         SpringApplication.run(RediSearchApplication.class, args);
     }
-
-}
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/articles")
-class ArticlesController {
-
-    private final JedisConnectionFactory cf;
-
-    @GetMapping("/search")
-    SearchResult search( //
-                         @RequestParam(name = "q") String query, //
-                         @RequestParam(name = "-1") Double minPrice, //
-                         @RequestParam(name = "-1") Double maxPrice) {
-
-        try (var client = new Client("articles-idx", cf.getHostName(), cf.getPort())) {
-            var q  = new Query(query);
-            if (minPrice != -1 && maxPrice != -1) {
-                q.addFilter(new Query.NumericFilter("price", minPrice, maxPrice));
-            }
-            q.returnFields("title", "price");
-            return client.search(q);
-        }
-    }
-
-    @GetMapping("/authors")
-    Collection<Suggestion> authorAutoComplete(@RequestParam(name = "q") String query) {
-        return null;
-    }
-}
-
-@Data
-@AllArgsConstructor
-@RedisHash
-class Author {
-
-    @Id
-    private String id;
-    private String name;
-}
-
-@Data
-@AllArgsConstructor
-@RedisHash
-class Article {
-
-    @Id
-    private String id;
-    private String title;
-    private Double price;
-
-    @Reference
-    private Set<Author> authors;
-
-    public void addAuthor(Author author) {
-        authors.add(author);
-    }
-}
-
-interface ArticleRepository extends CrudRepository<Article, String> {}
-interface AuthorRepository extends CrudRepository<Author, String> {}
-
-enum ArticleType {
-    INFLUENCE, LEARN, COOK, TEACH
 }
--- src/main/java/com/redislabs/edu/redisearch/demo/config/JedisConfiguration.java
+++ src/main/java/com/redislabs/edu/redisearch/demo/config/JedisConfiguration.java
@@ -1,14 +1,22 @@
 package com.redislabs.edu.redisearch.demo.config;
 
+import com.github.javafaker.Faker;
+import com.redislabs.edu.redisearch.demo.model.ArticleType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
 
 @Configuration
 public class JedisConfiguration {
 
+    @Autowired
     RedisProperties redisProperties;
 
     public JedisConfiguration(RedisProperties redisProperties) {
@@ -16,9 +24,53 @@ public class JedisConfiguration {
     }
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
+    public JedisConnectionFactory jedisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(
                 redisProperties.getHost(), redisProperties.getPort());
         return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+
+        return template;
+    }
+
+    static Map<ArticleType, String> titles = Map.of(
+            ArticleType.INFLUENCE, "The influence of %s on %s", //
+            ArticleType.LEARN, "What I learn %s from %s", //
+            ArticleType.COOK, "How to cook %s with %s", //
+            ArticleType.TEACH, "Teach your %s how to pilot a %s " //
+    );
+
+    public static String getFakeArticleTitle(Faker faker) {
+        var keys = titles.keySet();
+        var rando = (long) (keys.size() * Math.random());
+        var key = keys.stream().skip(rando).findAny().get();
+        var param1 = "";
+        var param2 = "";
+        switch (key) {
+            case INFLUENCE:
+                param1 = faker.ancient().god();
+                param2 = faker.food().dish();
+                break;
+            case LEARN:
+                param1 = faker.artist().name();
+                param2 = faker.backToTheFuture().character();
+                break;
+            case COOK:
+                param1 = faker.food().dish();
+                param2 = StringUtils.capitalize(faker.animal().name());
+                break;
+            case TEACH:
+                param1 = faker.animal().name();
+                param2 = faker.aviation().aircraft();
+                break;
+        }
+        return String.format(titles.get(key), param1, param2);
     }
 }

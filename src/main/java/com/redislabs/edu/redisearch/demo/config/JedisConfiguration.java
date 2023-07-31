746c4672f3076573100ab1551f637a8a06dd8cba
Rume7 <electengrhume@gmail.com>
2023-07-31 19:53:19 +0100

    Created config classes

--- src/main/java/com/redislabs/edu/redisearch/demo/config/JedisConfiguration.java
+++ src/main/java/com/redislabs/edu/redisearch/demo/config/JedisConfiguration.java
@@ -1,9 +1,24 @@
 package com.redislabs.edu.redisearch.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 @Configuration
 public class JedisConfiguration {
 
+    RedisProperties redisProperties;
 
+    public JedisConfiguration(RedisProperties redisProperties) {
+        this.redisProperties = redisProperties;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(
+                redisProperties.getHost(), redisProperties.getPort());
+        return new JedisConnectionFactory(config);
+    }
 }
